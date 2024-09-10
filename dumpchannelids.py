4121b89f048f30de859504abd1fb60e969be744c
ishnz <121901903+ishnz@users.noreply.github.com>
2024-09-10 17:30:03 +0200

    more safety checks

--- dumpchannelids.py
+++ dumpchannelids.py
@@ -1,6 +1,9 @@
 import json
 import os
 
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
 def change_to_package_dir():
     os.chdir("messages")
 
@@ -10,12 +13,47 @@ def format_ids(channel_ids):
         new.append("c" + id)
     return new
 
+def check_ids(ids):
+    change_to_package_dir()
+    cls()
+    existing = []
+    not_existing = []
+    for id in ids:
+        if os.path.exists(id):
+            existing.append(id)
+            continue;
+        else:
+            not_existing.append(id)
+    if not_existing is not []:
+        print("The following channel IDs do not exist:")
+        id_strings = ', '.join(not_existing)
+        print(id_strings)
+        print("Do you wish to continue and exclude those? (y/n)")
+        response = input()
+        if response == "n":
+            exit()
+        else:
+            return existing            
+
+def check_for_duplicates(ids):
+    seen = set()
+    result = []
+    for id in ids:
+        if id not in seen:
+            seen.add(id)
+            result.append(id)
+        else:
+            print("Duplicate channel ID found: " + id + " Removing...")
+    return result
+        
+
 def get_channel_ids():
     print("Enter the text file containing the channel IDs.")
     channel_ids_file = input()
     with open(channel_ids_file, 'r') as f:
         channel_ids = f.read().splitlines()
     channel_ids = format_ids(channel_ids)
+
     return channel_ids
 
 def dump_channel(id, output_file):
@@ -33,7 +71,8 @@ def dump_channel(id, output_file):
 
 def dump_all(ids):
     output_file = open("messages.txt", "w")
-    change_to_package_dir()
+    ids = check_ids(ids)
+    ids = check_for_duplicates(ids)
     for id in ids:
         dump_channel(id, output_file)
 
